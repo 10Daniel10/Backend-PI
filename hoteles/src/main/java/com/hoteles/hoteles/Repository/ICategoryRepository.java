18dd195082baada337a08aaf36092eea786c5288
Jairo Rivera Lopez <jairorive96@gmail.com>
2022-10-18 21:27:41 -0500

    creating packages for the project structure

--- hoteles/src/main/java/com/hoteles/hoteles/Repository/ICategoryRepository.java
+++ hoteles/src/main/java/com/hoteles/hoteles/Repository/ICategoryRepository.java
@@ -1,2 +1,7 @@
-package com.hoteles.hoteles.Repository;public interface ICategoryRepository {
+package com.hoteles.hoteles.Repository;
+
+import com.hoteles.hoteles.Entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ICategoryRepository extends JpaRepository<Category, Long> {
 }
